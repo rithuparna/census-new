7a94071a77c5615c4184b6bc2a742cd7576e2fe3
rithuparna <99005818+rithuparna@users.noreply.github.com>
2022-07-08 20:37:40 +0530

    Add files via upload

---
+++ home.py
@@ -0,0 +1,15 @@
+# Show complete dataset and summary in 'census_home.py'
+# Import necessary modules.
+import numpy as np
+import pandas as pd
+import streamlit as st
+# Define a function 'app()' which accepts 'census_df' as an input.
+def app(census_new):
+    st.header("View Data")
+    # Display dataset within beta_expander.
+    with st.beta_expander("View Dataset"):
+        st.table(census_df)    
+    # Show dataset summary on click of a checkbox.
+    st.subheader("Columns Description:")
+    if st.checkbox("Show summary"):
+        st.table(census_df.describe())
